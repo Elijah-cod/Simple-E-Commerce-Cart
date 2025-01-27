d650dfcf32d923edb9745fb309e058672d93aa88
Elijah <iamngunjiri@gmail.com>
2025-01-27 16:19:10 +0300

    The cart items can be removed while also taking into consideration the Quantity

--- vite-project/src/OpenCart.jsx
+++ vite-project/src/OpenCart.jsx
@@ -1,12 +1,28 @@
-function OpenCart ({cart, total}) {
-    console.log(cart)
+import { faX } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function OpenCart ({cart, total, setCart}) {
+    function removeItem(id) {
+        console.log(id)
+        const updatedData = cart
+                .map(item => {
+                    if (item.id === id) {
+                    return { ...item, quantity: item.quantity - 1 }; // Reduce quantity by 1
+                    }
+                    return item;
+                })
+                .filter(item => item.quantity > 0); // Remove items with quantity <= 0
+
+                setCart(updatedData)
+        console.log(cart);
+    }
     return(
         <>
             <h2>Cart</h2>
             <ol>
                 {
                     cart.map((item)=>(
-                        <li key={item.id}>Item: {item.name} Price: $ {item.price} Quantity: {item.quantity}</li>
+                        <li key={item.id}>{item.name}, Price: $ {item.price} Quantity: {item.quantity}<FontAwesomeIcon className="list" onClick={()=>removeItem(item.id)} icon={faX} /></li>
                     ))
                 }
             </ol>
